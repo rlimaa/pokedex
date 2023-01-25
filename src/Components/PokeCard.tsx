8aba9ebc027aa2ff82f06c0f52ae1b31febfa12a
rodrigo_araujo42 <devcontractor_ra@bybox.com>
2023-01-25 09:28:22 -0300

    fancy images

--- src/Components/PokeCard.tsx
+++ src/Components/PokeCard.tsx
@@ -5,10 +5,11 @@ import "./PokeCard.css"
 class PokeCard extends Component<PokeCardProps, {}> {
     render() {
         const { id, name, type, base_experience } = this.props.Pokemon;
+        const url = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${('000' + id).substring(('000'+id).length - 3 )}.png`;
         return (
             <div className="Pokecard">
                 <p className="Pokecard-title">{name}</p>
-                <img className="Pokecard-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={`${name}`} />
+                <img className="Pokecard-image"  alt={name} src={url} />
                 <p className="Pokecard-data">Type: {type}</p>
                 <p className="Pokecard-data">EXP: {base_experience}</p>
             </div>
